86e48998ae48f232a61135adff1c76179fe7326d
abousiyd <abdou.bousiyd@gmail.com>
2021-08-09 19:58:24 +0200

    add components in frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+// import './App.css';
+import Navigation from './components/Navigation'
+import NotesList from './components/NotesList'
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser';
+
+function App() {
+  return (
+    <div>
+    <Router>
+      <Navigation />
+
+      <Route path='/' exact component={NotesList} />
+      <Route path='/edit/:id' component={CreateNote} />
+      <Route path='/create' component={CreateNote} />
+      <Route path='/user' component={CreateUser} />
+
+    </Router>
+
+    </div>
+  )
+}
+
+export default App;
